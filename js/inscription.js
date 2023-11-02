ad1f25e6de42d028bab4556146f9da90a208761f
장민규 <2018009034@hanyang.ac.kr>
2023-11-02 11:26:40 +0900

    adf

--- js/inscription.js
+++ js/inscription.js
@@ -801,8 +801,6 @@ function save()
   .then((result) => {
     getDeMoList(user.uid)
     localStorage.setItem("cnt", cnt+1)
-    var cnt = localStorage.getItem("cnt")
-    
     demo_image(cnt)
     demo_status(cnt)
     console.log(result)});
